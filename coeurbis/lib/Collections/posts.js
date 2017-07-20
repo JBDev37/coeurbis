3a6b168022a3b25608825a483a12f70e62dc6360
JB DEV <jbroussat@orange.fr>
2017-07-20 07:29:56 +0200

    Installation de Admin
    Installation de l'interface Admin (fonctionne)

--- coeurbis/lib/Collections/posts.js
+++ coeurbis/lib/Collections/posts.js
@@ -1,18 +1,4 @@
 Posts = new Mongo.Collection('posts');
 
 
-AdminConfig = {
-  collections: {
-    Posts: {}
-  }
-};
-
-
-AdminConfig = {
-  name: 'My App',
-  adminEmails: ['ben@code2create.com'],
-  collections: {
-    Posts: {}
-  },
-};
 
